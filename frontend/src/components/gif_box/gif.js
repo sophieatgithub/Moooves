2c4a0ce4303a9376e74eaf1669392c849f754c8e
sophieatgithub <sophie@bajescu.com>
2020-08-12 20:43:50 +0300

    creating first component using atomic design

---
+++ frontend/src/components/gif_box/gif.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import filler from '../media/filler.jpeg';
+
+function Gif() {
+    return(
+        <div>
+            <img src={filler} alt="filler image" />        
+        </div>
+    )
+}
+
+export default Gif
