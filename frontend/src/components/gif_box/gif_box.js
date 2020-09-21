4487d72a5c1209759e78251d867431749e8b9ea8
sophieatgithub <sophie@bajescu.com>
2020-09-21 23:13:02 +0300

    aligning text and gif

--- frontend/src/components/gif_box/gif_box.js
+++ frontend/src/components/gif_box/gif_box.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { startTime } from './youtube_link';
 import Title from './title';
 import Gif from './gif1';
 import Tags from './tags';
@@ -17,8 +16,10 @@ function GifBox(props) {
                 <a href={move.videos[0].url + "&t=" + Number(Number((move.videos[0].start.split(":")[0]*60))+Number(move.videos[0].start.split(":")[1]))} target="_blank">
                     <Gif gif={move.thumbnail} />
                 </a>
-                    <Title title={move.title} />
-                    <Tags tag={move.tags} />
+                    <div className="titlentag_container">
+                        <Title title={move.title} />
+                        <Tags tag={move.tags} />
+                    </div>
                 </div>
             ))}
         </>
